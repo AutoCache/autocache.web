0c501b9b5bb2b509d469c7f279400c3b4fb9bfab
Ozgur Ozturk <ozgur7@gmail.com>
2013-03-23 17:26:19 -0400

    Added seeds for users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,33 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create({
-  email: "cajun.code@gmail.com", 
+  email: "veronica@autocache.com", 
   password: "testpass1",
   password_confirmation: "testpass1", 
-  authentication_token: ""})
+  authentication_token: "370700003501d3"})
+  
+User.create({
+  email: "ozgur@autocache.com", 
+  password: "testpass1",
+  password_confirmation: "testpass1", 
+  authentication_token: "370700003501b3"})
+
+User.create({
+  email: "bill@autocache.com", 
+  password: "testpass1",
+  password_confirmation: "testpass1", 
+  authentication_token: "370700003501b9"})
+
+User.create({
+  email: "allen@autocache.com", 
+  password: "testpass1",
+  password_confirmation: "testpass1", 
+  authentication_token: "37070000350199"})  
+  
+User.create({
+  email: "default@autocache.com", 
+  password: "testpass1",
+  password_confirmation: "testpass1", 
+  authentication_token: "37070000350047"})  
+  
+    
